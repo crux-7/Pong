40135b05cc33736860b1c11dcbda7b557f46b3b3
crux-7 <44284219+crux-7@users.noreply.github.com>
2018-10-21 00:40:02 +0530

    Initial Commit

---
+++ Ball.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    private Rigidbody2D ball;
+    private Vector2 initialForce = new Vector2(-4f, 0.5f);
+    private Vector2 randomInitialForce;
+    // Use this for initialization
+    void Start () {
+        randomInitialForce = new Vector2(-4f, Random.Range(0.5f, 1.5f));
+        ball = gameObject.GetComponent<Rigidbody2D>();
+        ball.AddForce(randomInitialForce, ForceMode2D.Impulse);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private GameObject player;
+    private GameObject player2;
+    private float smoothner = 0.2F;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        player2 = GameObject.FindGameObjectWithTag("Player 2");
+    }
+
+    void Update()
+    {
+
+        //Keyboard Controls
+        if (Input.GetAxis("Player 1") > 0)
+        {
+            player.transform.Translate(0f, Input.GetAxis("Player 1") * smoothner, 0f);      
+        }
+
+        if (Input.GetAxis("Player 1") < 0)
+        {
+            player.transform.Translate(0f, Input.GetAxis("Player 1") * smoothner, 0f);
+        }
+
+        if (Input.GetAxis("Player 2") > 0)
+        {
+            player2.transform.Translate(0f, Input.GetAxis("Player 2") * smoothner, 0f);
+        }
+
+        if (Input.GetAxis("Player 2") < 0)
+        {
+            player2.transform.Translate(0f, Input.GetAxis("Player 2") * smoothner, 0f);
+        }
+
+        //Mobile Controls
+        if (Input.touchCount > 0)
+        {
+            var touch = Input.GetTouch(0);
+
+            if (touch.position.x < Screen.width / 2)
+            { 
+                player.transform.Translate(0f, Input.GetAxis("Player 1") * smoothner, 0f);
+            }
+            else if (touch.position.x > Screen.width / 2)
+            {
+                player.transform.Translate(0f, Input.GetAxis("Player 1") * smoothner, 0f);
+            }
+        }
+
+    }
+}
